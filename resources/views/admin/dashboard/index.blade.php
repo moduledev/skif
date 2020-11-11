3ae6450383e004db884330fa632678421c821072
lion1121 <bayduzh89@gmail.com>
2020-11-11 13:46:12 +0200

    spatie permission instaled, created AdminController

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,9 +1,1 @@
-<p>123</p>
-
-<a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-    Logout
-</a>
-
-<form id="logout-form" action="{{ route('logout') }}" method="POST">
-    @csrf
-</form>
+@extends('admin.layouts.app')
